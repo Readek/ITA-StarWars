9caf9502ac5851d27ff662796179caeae8c68418
Readek <readeku@gmail.com>
2024-12-02 12:41:18 +0100

    Fix Firebase env variable

--- src/firebase/firebase.mjs
+++ src/firebase/firebase.mjs
@@ -1,7 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = process.env.firebaseConfig;
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain: "ita-sprint6.firebaseapp.com",
+    projectId: "ita-sprint6",
+    storageBucket: "ita-sprint6.firebasestorage.app",
+    messagingSenderId: "637341146514",
+    appId: "1:637341146514:web:2f1596c6dda2c994b2b54b"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
